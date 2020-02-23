d9b63ede3482cbc55760b02536c37f68681bc394
Enrique Benitez <enriquebenitezjr@gmail.com>
2020-02-23 15:19:13 -0600

    Create client

---
+++ client/src/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default () => (
+  <div>
+    This project was created to learn about docker multi-container applications
+    <Link to="/">Go back home</Link>
+  </div>
+)
